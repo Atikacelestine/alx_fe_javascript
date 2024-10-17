30a65d4059a72dac458c21e147b71188cddb4d8f
Atikacelestine <celestineatika313@gmail.com>
2024-10-17 07:24:56 +0300

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -43,3 +43,13 @@ function createAddQuoteForm() {
 //Event listener for the Add quote button
 document.getElementById("CreateaddQuoteButton").addEventListener("click",addQuote);
 }
+ function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
