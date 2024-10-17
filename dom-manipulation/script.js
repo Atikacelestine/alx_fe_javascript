8eca4971f82949becbaf3740d1fc05ce4a98ce8d
Atikacelestine <celestineatika313@gmail.com>
2024-10-17 07:45:18 +0300

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -7,6 +7,14 @@ const quotes=[
     {text:"Believe you can and you are almost there it just a matter of time",category:"Positivity"}
 ];
 
+//Array to hold quotes,initialize them from the local storage when available
+let quotes= 
+    JSON>parse(localStorage.getItem('quotes)) ||[
+                                    {text:"The only way to do good work is to love what you do.",category:"Motivation"},
+                                    {text:"Success is not the final,failure is not the end of hope .It is te courage to face things and do you're best to find solution",category:"Inspiration"},
+    {text:"Believe you can and you are almost there it just a matter of time",category:"Positivity"}
+];
+
 //Display random Quotes 
 function showRandomQuote(){
     const quoteDisplay= document.getElementById("quoteDisplay");
@@ -20,6 +28,9 @@ background-color:#f9f9f9;">
 font-weight:bold;">- <span>$ {quote.category}</span></p>
 </div>
 `;
+
+//Save the last viewed quote in your session
+    sessionStorage.setItem('lastQuote',JSON.stringify(quote));
     //clear previous quotes
     randomQuote.innerHTML="";
 }
@@ -31,7 +42,7 @@ function createAddQuoteForm() {
     const newQuoteText= document.getElementById("newQuoteCategory").value;
     if(newQuoteText && newQuoteCategory) {
         quotes:push({ text :newQuoteText, category: newQuoteCategory});
-        document.getElementById("newQuoteText").value-"";//Clear input fields
+        document.getElementById("newQuoteText").value="";//Clear input fields
 
         document.getElementById("newQuoteCategory").value="";
         alert("New quote adde successfully!");
@@ -39,10 +50,26 @@ function createAddQuoteForm() {
         alert("Please fill i both the quote text and category.");
     }
     document.body.appendChild(createElement);
+
+    //Save the updated quotes array to local storage
+    localStorage.setItem('quotes', JSON.stringify(quotes));
 }
 //Event listener for the Add quote button
 document.getElementById("CreateaddQuoteButton").addEventListener("click",addQuote);
 }
+//Dynamically create the form when the page loads
+window.onload=fnction(){
+createAddQuoteForm();
+    const lastQuote=
+        JSON.parse(sessionStorage.getItem('last Quote'));
+    if(lastQuote){
+        const quoteDisplay=document.getElementById("quoteDisplay");
+        quoteDisplay.innerHTML= `
+        <div style="border: 1px solid #ccc;
+        </div>
+        }
+        };
+        
  function importFromJsonFile(event) {
     const fileReader = new FileReader();
     fileReader.onload = function(event) {
